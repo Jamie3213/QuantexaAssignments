4bf9fcb01b26fb3e2cc6a6660dd2ea33b4687411
Jamie Hargreaves <41867515+Jamie3213@users.noreply.github.com>
2021-08-07 19:18:00 +0000

    Complete question 2 of transactions assignment

--- src/main/scala/com/quantexa/assignments/TransactionAssignment.scala
+++ src/main/scala/com/quantexa/assignments/TransactionAssignment.scala
@@ -28,8 +28,8 @@ object TransactionAssignment {
         /* -------------------- Transform data and produce output ------------------- */
 
         // Question 1
-        // val question1ResultValue = getTransactionTotalsByDay(transactions)
-        // question1ResultValue.foreach(println)
+        val question1ResultValue = getTransactionTotalsByDay(transactions)
+        question1ResultValue.foreach(println)
 
         // Question 2
         val question2ResultValue = getAverageTransactionsByAccountAndType(transactions)
